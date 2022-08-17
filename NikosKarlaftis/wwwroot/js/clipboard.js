b1a8f5606e73fd73d8f066db2d31bfa11a15bd6e
NickKarlaftis <nickfromcrete@hotmail.com>
2022-08-17 17:54:03 +0300

    Bootstrap commit

---
+++ NikosKarlaftis/wwwroot/js/clipboard.js
@@ -0,0 +1,12 @@
+﻿{
+    window.clipboardCopy = {
+        copyText: function (text) {
+            navigator.clipboard.writeText(text).then(function () {
+                alert("Copied to clipboard!");
+            })
+                .catch(function (error) {
+                    alert(error);
+                });
+        }
+    };
+}
